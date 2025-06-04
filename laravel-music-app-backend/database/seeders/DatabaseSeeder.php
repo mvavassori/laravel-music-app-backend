d64dc8d6629565b00162a68b5ba89cca1f96c331
Marco Vavassori <mv@marcovavassori.com>
2025-06-04 10:30:10 +0200

    seed db

--- laravel-music-app-backend/database/seeders/DatabaseSeeder.php
+++ laravel-music-app-backend/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Song;
+use App\Models\Artist;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // laravel collection containing 5 Artist model instances
+        $artists = Artist::factory(5)->create();
+
+        // loops through the artists collection and create 2 to 5 new songs per artist 
+        foreach ($artists as $artist) {
+            Song::factory(rand(2, 5))->create([
+                'artist_id' => $artist->id  // we specified the artist_id in the attributes otherwise it would have created 2 to 5 new artists at each iteration 
+            ]);
+        }
+        
     }
 }
