02a12e42aea229bb785f4e6b9db00dbf3d98e1f5
Marco Vavassori <mv@marcovavassori.com>
2025-06-06 09:58:53 +0200

    add middleware for json force responses

--- laravel-music-app-backend/app/Providers/RouteServiceProvider.php
+++ laravel-music-app-backend/app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware(['api','jsonForce'])
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
--- laravel-music-app-backend/routes/api.php
+++ laravel-music-app-backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlbumController;
+use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
