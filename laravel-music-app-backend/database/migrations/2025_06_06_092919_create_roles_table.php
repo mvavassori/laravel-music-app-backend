6123def9ddb5d67a0b4a11c698695667a04250e7
Marco Vavassori <mv@marcovavassori.com>
2025-06-07 14:43:36 +0200

    made roles unique

--- laravel-music-app-backend/database/migrations/2025_06_06_092919_create_roles_table.php
+++ laravel-music-app-backend/database/migrations/2025_06_06_092919_create_roles_table.php
@@ -4,16 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
@@ -21,8 +19,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('roles');
     }
 };
