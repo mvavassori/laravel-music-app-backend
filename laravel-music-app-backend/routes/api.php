b263ef3447d39170554ddaf1c4448c86895089ad
Marco Vavassori <mv@marcovavassori.com>
2025-06-08 22:55:17 +0200

    added routes

--- laravel-music-app-backend/routes/api.php
+++ laravel-music-app-backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\AlbumController;
-use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
@@ -28,34 +28,46 @@ $myFunc = fn() => $someVar;
 // Route::get("/users/{id}", $myFunc);
 
 
-
 // Artist routes
-Route::prefix('v1/artists')->name('api.v1.')->group(function () {
-    Route::post('/', [ArtistController::class, 'store'])->name('artists.store')->middleware('throttle:60,1'); // 60 requests per minute max
-    Route::get('/', [ArtistController::class, 'index'])->name('artists.index');
-    Route::get('/{id}', [ArtistController::class, 'show'])->name('artists.show');
-    Route::get('/{id}/songs', [ArtistController::class, 'showWithSongs'])->name('artists.show.songs');
-    Route::get('/{artist}/albums', [ArtistController::class, 'showWithAlbums'])->name('artists.show.albums'); // made it to test laravel route model binding
-    Route::put('/{id}', [ArtistController::class, 'update'])->name('artists.update');
-    Route::delete('/{id}', [ArtistController::class, 'destroy'])->name('artists.destroy');
+Route::prefix('v1/artists')->name('api.v1.artists.')->group(function () {
+    Route::get('/', [ArtistController::class, 'index'])->name('index');
+    Route::post('/', [ArtistController::class, 'store'])->name('store')->middleware('throttle:60,1');
+    Route::get('/{id}', [ArtistController::class, 'show'])->name('show');
+    Route::get('/{id}/contributions', [ArtistController::class, 'showWithContributions'])->name('show.contributions');
+    Route::get('/{id}/songs', [ArtistController::class, 'showWithSongs'])->name('show.songs');
+    Route::get('/{id}/albums', [ArtistController::class, 'showWithAlbums'])->name('show.albums');
+    Route::put('/{id}', [ArtistController::class, 'update'])->name('update');
+    Route::delete('/{id}', [ArtistController::class, 'destroy'])->name('destroy');
 });
 
 // Song routes
-Route::prefix('v1/songs')->name('api.v1.')->group(function () {
-    Route::post('/', [SongController::class, 'store'])->name('songs.store');
-    Route::get('/{id}', [SongController::class, 'show'])->name('songs.show');
-    Route::get('/{id}/artists', [SongController::class, 'showWithArtists'])->name('songs.show.artists');
-    Route::get('/{id}/album', [SongController::class, 'showWithAlbum'])->name('songs.show.album');
-    Route::put('/{id}', [SongController::class, 'update'])->name('songs.update');
-    Route::delete('/{id}', [SongController::class, 'destroy'])->name('songs.destroy');
+Route::prefix('v1/songs')->name('api.v1.songs.')->group(function () {
+    Route::post('/', [SongController::class, 'store'])->name('store')->middleware('throttle:60,1');
+    Route::get('/{id}', [SongController::class, 'show'])->name('show');
+    Route::get('/{id}/contributions', [SongController::class, 'showWithContributions'])->name('show.contributions');
+    Route::get('/{id}/artists', [SongController::class, 'showWithArtists'])->name('show.artists');
+    Route::get('/{id}/album', [SongController::class, 'showWithAlbum'])->name('show.album');
+    Route::get('/{id}/complete', [SongController::class, 'showComplete'])->name('show.complete');
+    Route::put('/{id}', [SongController::class, 'update'])->name('update');
+    Route::delete('/{id}', [SongController::class, 'destroy'])->name('destroy');
 });
 
 // Album routes
-Route::prefix('v1/albums')->name('api.v1.')->group(function () {
-    Route::post('/', [AlbumController::class, 'store'])->name('albums.store');
-    Route::get('/{id}', [AlbumController::class, 'show'])->name('albums.show');
-    Route::get('/{id}/songs', [AlbumController::class, 'showWithSongs'])->name('albums.show.songs');
-    Route::get('/{id}/artists', [AlbumController::class, 'showWithArtists'])->name('albums.show.artists');
-    Route::put('/{id}', [AlbumController::class, 'update'])->name('albums.update');
-    Route::put('/{id}', [AlbumController::class, 'destroy'])->name('albums.destroy');
+Route::prefix('v1/albums')->name('api.v1.albums.')->group(function () {
+    Route::post('/', [AlbumController::class, 'store'])->name('store')->middleware('throttle:60,1');
+    Route::get('/{id}', [AlbumController::class, 'show'])->name('show');
+    Route::get('/{id}/songs', [AlbumController::class, 'showWithSongs'])->name('show.songs');
+    Route::get('/{id}/contributions', [AlbumController::class, 'showWithContributions'])->name('show.contributions');
+    Route::get('/{id}/complete', [AlbumController::class, 'showComplete'])->name('show.complete');
+    Route::put('/{id}', [AlbumController::class, 'update'])->name('update');
+    Route::delete('/{id}', [AlbumController::class, 'destroy'])->name('destroy');
+});
+
+// Role routes
+Route::prefix('v1/roles')->name('api.v1.roles.')->group(function () {
+    Route::get('/', [RoleController::class, 'index'])->name('index');
+    Route::post('/', [RoleController::class, 'store'])->name('store')->middleware('throttle:60,1');
+    Route::get('/{id}', [RoleController::class, 'show'])->name('show');
+    Route::get('/name/{name}', [RoleController::class, 'showByName'])->name('show.byName');
+    Route::delete('/{id}', [RoleController::class, 'destroy'])->name('destroy');
 });
